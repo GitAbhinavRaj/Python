5c77a317858f08c57b42cc03bc9b40beb2701ccd
abhi9 <abhinavrajsingh@hotmail.com>
2023-09-03 20:27:24 +0530

    Python Day 1

---
+++ Day1/HelloWorld.py
@@ -0,0 +1,25 @@
+#DAY 1 
+
+print("Hello world")     # hello world is a string because it is in "  ".
+print("567")
+
+print("Hello Python \nThis is for new line.\n")
+print("Hello \"Python\" \nThis is for new line.")
+print('Hello "Python" \nThis is for new line.\n')
+print("hello","python",5,6,7,sep="-",end=" ENDLINE")  # Default separator(sep) space
+
+
+
+"""
+Python is a popular programming language.
+It was created by Guido van Rossum, and released in 1991.
+
+It is used for:
+
+1. web development (server-side)
+2. software development
+3. mathematics
+4. system scripting.
+5. GUI Creation.
+6. Web scrapping.
+"""
