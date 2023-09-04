319fdbebdca6d646085bc4c1f0f3ea52a9b12797
abhi9 <abhinavrajsingh@hotmail.com>
2023-09-05 00:24:25 +0530

    Variable and Operotor

---
+++ Day1/VariableAndOperator.py
@@ -0,0 +1,21 @@
+# Variable : that can hold data
+#In python everything is Object
+a = 1
+b = 2.345
+c = "Python"
+d = True
+e = None
+f = complex(11,2)
+print(a,b,c,d,e,f,sep="\n")
+
+#Operator
+#  +,-,*,/,%,//,**
+
+print(15+10)  #add
+print(15-10)  #substract
+print(15*10)  #Multiplication
+print(15/10)  #divide
+print(15//10) #floor
+print(2**3)   #power
+
+
